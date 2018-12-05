2df60444903a9d71f51891649974302611b21545
Rodrigo Gomes de Souza <rodrigo.gomes@amazonas.gov.br>
2018-12-05 10:01:23 -0400

    Arquivos iniciais

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PAGINA INICIAL</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<header class="navbar">
+		<a href="#">Home</a>
+		<div class="dropdown">
+			<a class="dropbtn" href="#">Html</a>
+			<div class="dropdown-content">
+				<a href="#">Tutorial 1</a>
+				<a href="#">Tutorial 2</a>
+			</div>	
+		</div>
+		<a href="#">CSS</a>
+	</header>
+</body>
+</html>
